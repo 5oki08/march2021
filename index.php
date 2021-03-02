733e2a530e312b1c95d22c76a2a1f6e9875de3cb
joasem <rollingstone>
2021-03-02 08:54:18 +0300

    movement0854hrs

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>intro PHP</title>
+
+<!-- local link -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script type="text/javascript" src="js/style.js"></script>
+
+<!-- bootstrap local link -->
+<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.grid.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.grid.min.css">
+<!-- js local link -->
+<script type="text/javascript" src="bootstrap/bootstrap.js"></script>
+<script type="text/javascript" src="bootstrap/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+</body>
+</html>
