9f2e7091504e5af84fe6b9d018bdf99c63f64d52
joasem <rollingstone>
2021-03-02 11:26:53 +0300

    a  lil catchup (php)

--- index.php
+++ index.php
@@ -10,14 +10,52 @@
 <!-- bootstrap local link -->
 <link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.grid.css">
-<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.grid.min.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap-grid.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap-grid.min.css">
 <!-- js local link -->
 <script type="text/javascript" src="bootstrap/bootstrap.js"></script>
 <script type="text/javascript" src="bootstrap/bootstrap.min.js"></script>
 
 </head>
 <body>
+
+<div id="firstheader">
+
+
+	<?php 
+
+	// data display using echo
+	echo "first page <br/>" ;
+
+
+	// using variables to display data
+	$a = "porsche" ;
+	$b = "bugatti" ;
+	$c = "mobius" ;
+	$d = 4 ;
+	echo "In Kenya" . " " . "$c" . " " . "is locally manufactured and assembled." ;
+
+	echo " <br/> " ;
+
+	$e = 8562 ;
+	var_dump (is_int ($e) ) ;
+
+	echo "<br/>";
+
+	$f = 8562.2 ;
+	var_dump (is_float ($f) ) ;
+
+	echo = "<br/>" ;
+
+
+
+	?>
+
+
+</div>
+
+
+
 
 </body>
 </html>
